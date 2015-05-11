b3ba8eb61a19fd4b2b68309094182ae021b7bd37
mindwing <mindwing@gmail.com>
2015-05-11 11:02:29 +0900

    SMS 전송 기본기능 완료

--- SMS/src/kr/mindwing/sms/MainActivity.java
+++ SMS/src/kr/mindwing/sms/MainActivity.java
@@ -13,11 +13,39 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final String ACTION_DELIVERY = "delivery";
+	private static final String ACTION_SENT = "sent";
+
+	private TextView tel;
+	private TextView body;
 	private Button sendButton;
+
+	private BroadcastReceiver sentReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String msg = getResultString(getResultCode());
+
+			Toast.makeText(MainActivity.this, "sent: " + msg,
+					Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	private BroadcastReceiver deliveryReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String msg = getResultString(getResultCode());
+
+			Toast.makeText(MainActivity.this, "delivery: " + msg,
+					Toast.LENGTH_SHORT).show();
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,92 +58,67 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				SmsManager manager = SmsManager.getDefault();
-				manager.sendTextMessage("01020242501", "01026602501", "테스트",
-						PendingIntent
-								.getBroadcast(MainActivity.this, 2501,
-										new Intent("sent"),
-										PendingIntent.FLAG_ONE_SHOT),
-						PendingIntent.getBroadcast(MainActivity.this, 2502,
-								new Intent("delivery"),
-								PendingIntent.FLAG_ONE_SHOT));
+				manager.sendTextMessage(tel.getText().toString(), null, body
+						.getText().toString(), PendingIntent.getBroadcast(
+						MainActivity.this, 2501, new Intent(ACTION_SENT),
+						PendingIntent.FLAG_ONE_SHOT), PendingIntent
+						.getBroadcast(MainActivity.this, 2502, new Intent(
+								ACTION_DELIVERY), PendingIntent.FLAG_ONE_SHOT));
 			}
 		});
 
-		registerReceiver(new BroadcastReceiver() {
+		tel = (TextView) findViewById(R.id.text_tel);
+		body = (TextView) findViewById(R.id.text_body);
+	}
 
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				String msg;
+	protected void onResume() {
+		super.onResume();
 
-				switch (getResultCode()) {
-				case Activity.RESULT_OK:
-					msg = "SMS sent, RESULT_OK";
-					break;
-
-				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-					msg = "SMS sent, RESULT_ERROR_GENERIC_FAILURE";
-					break;
-
-				case SmsManager.RESULT_ERROR_NO_SERVICE:
-					msg = "SMS sent, RESULT_ERROR_NO_SERVICE";
-					break;
-
-				case SmsManager.RESULT_ERROR_NULL_PDU:
-					msg = "SMS sent, RESULT_ERROR_NULL_PDU";
-					break;
-
-				case SmsManager.RESULT_ERROR_RADIO_OFF:
-					msg = "SMS sent, RESULT_ERROR_RADIO_OFF";
-					break;
-
-				default:
-					msg = "알 수 없는 응답코드";
-					break;
-				}
-
-				Toast.makeText(MainActivity.this, "sent: " + msg,
-						Toast.LENGTH_SHORT).show();
-			}
-		}, new IntentFilter("sent"));
+		registerReceiver(sentReceiver, new IntentFilter(ACTION_SENT));
 
 		// XXX 우리나라 이통사들은 지원하지 않는 듯...
 		// http://stackoverflow.com/questions/15324977/android-smsmanager-sendtextmessage-i-get-sentintent-but-never-get-deliveryinte
-		registerReceiver(new BroadcastReceiver() {
+		registerReceiver(deliveryReceiver, new IntentFilter(ACTION_DELIVERY));
+	}
 
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				String msg;
+	@Override
+	protected void onPause() {
+		super.onPause();
 
-				switch (getResultCode()) {
-				case Activity.RESULT_OK:
-					msg = "SMS delivery, RESULT_OK";
-					break;
+		unregisterReceiver(sentReceiver);
+		unregisterReceiver(deliveryReceiver);
+	}
 
-				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-					msg = "SMS delivery, RESULT_ERROR_GENERIC_FAILURE";
-					break;
+	protected String getResultString(int resultCode) {
+		String msg;
 
-				case SmsManager.RESULT_ERROR_NO_SERVICE:
-					msg = "SMS delivery, RESULT_ERROR_NO_SERVICE";
-					break;
+		switch (resultCode) {
+		case Activity.RESULT_OK:
+			msg = "RESULT_OK";
+			break;
 
-				case SmsManager.RESULT_ERROR_NULL_PDU:
-					msg = "SMS delivery, RESULT_ERROR_NULL_PDU";
-					break;
+		case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+			msg = "RESULT_ERROR_GENERIC_FAILURE";
+			break;
 
-				case SmsManager.RESULT_ERROR_RADIO_OFF:
-					msg = "SMS delivery, RESULT_ERROR_RADIO_OFF";
-					break;
+		case SmsManager.RESULT_ERROR_NO_SERVICE:
+			msg = "RESULT_ERROR_NO_SERVICE";
+			break;
 
-				default:
-					msg = "알 수 없는 응답코드";
-					break;
-				}
+		case SmsManager.RESULT_ERROR_NULL_PDU:
+			msg = "RESULT_ERROR_NULL_PDU";
+			break;
 
-				Toast.makeText(MainActivity.this, "delivery: " + msg,
-						Toast.LENGTH_SHORT).show();
-			}
-		}, new IntentFilter("delivery"));
+		case SmsManager.RESULT_ERROR_RADIO_OFF:
+			msg = "RESULT_ERROR_RADIO_OFF";
+			break;
+
+		default:
+			msg = "알 수 없는 응답코드";
+			break;
+		}
+
+		return msg;
 	}
 
 	@Override
